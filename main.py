e00b72e9b47f6ed4e13f0764f946a3cb580e263c
rando010 <56077487+rando010@users.noreply.github.com>
2019-11-24 21:49:46 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 import csv
+
 candidate = ""
 voter_id = ""
-vote = ""
-Correy = 0
-Khan = 0
-Li = 0
-OTooley = 0
+vote=""
+Correyvote= 0
+Khanvote= 0
+Livote = 0
+OTooleyvote = 0
 
-with open('election_data.csv', 'r') as file:
-    csv_reader = csv.reader(file, delimiter=',')
-for line in csv_reader:
+file_to_open = "election_data.csv"
+
+with open(file_to_open, 'r') as this_csv_file:
+    csv_reader = csv.reader(this_csv_file, delimiter=',')      
     
-    data = [l for l in csv_reader]
-    voter_id = line[0]
-    vote = line[2]
-    if vote =="Correy":
-        Correyresult = Correy + 1
-    if vote =="Khan":
-        Khanresult = Khan + 1
-    if vote =="Li":
-        Liresult = Li +1
-    if vote =="OTooley": 
-        OTooleyresult = OTooley + 1
 
+    for line in csv_reader:    
+        voter_id = line[0]
+        vote = line[2]
+        #if voter_id==" * "
+            #number_of_votes_cast=number_of_votes_cast+1
+        if vote =="Correy":
+            Correyvote=Correyvote+1
+        if vote =="Khan":
+            Khanvote = Khanvote + 1
+        if vote =="Li":
+            Livote = Livote +1
+        if vote =="O'Tooley": 
+            OTooleyvote = OTooleyvote + 1
 
-    number_of_votes_cast = len(data)
-
+      
+  
+  
 print("Election Results")
-print("__________________")
-print("Total Votes:" + str(number_of_votes_cast))
-print("__________________")   
-print(Correyresult)
-print(Khanresult)
-print(Liresult)
-print(OTooleyresult)
+print("______________________")
+#print("Total Votes:" + str(number_of_votes_cast))
+print("______________________")   
+print("Correy " + str(Correyvote))
+print("Khan " +str(Khanvote))
+print("Li " +str(Livote))
+print("Otooley " +str(OTooleyvote))
+print("______________________") 
+if (Correyvote>Khanvote) and (Correyvote>Livote) and (Correyvote>OTooleyvote):
+    print("Winner: Correy")
+if (Khanvote>Correyvote) and (Khanvote>Livote) and (Khanvote>OTooleyvote):
+    print("Winner: Khan")
+if (Livote>Correyvote) and (Livote>Khanvote) and (Livote>OTooleyvote):
+    print("Winner: Li") 
+if (OTooleyvote>Correyvote) and (OTooleyvote>Khanvote) and (OTooleyvote>Livote):
+    print("Winner: OTooley")           
